d66e3294f9d7171008cdb5b9dd04dafe405d5bbe
GeorgiStanev92 <georgistanev92@gmail.com>
2021-09-21 16:38:34 +0300

    AuthRepository (Register, UserExist, Login )
    making authentications for Users to register, login or does exist

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using dotnet_rpg.Data;
+using dotnet_rpg.DTO.User;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDTO request)
+        {
+            var response = await _authRepo.Register(
+                new User { Username = request.Username }, request.Password
+            );
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLoginDTO request)
+        {
+            var response = await _authRepo.Login(
+                request.Username, request.Password
+            );
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
---
+++ DTO/User/UserRegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.DTO.User
+{
+    public class UserRegisterDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
